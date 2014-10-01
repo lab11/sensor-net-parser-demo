478d1307a813bbea13f0981d7fd051a909d54d4f
Rohit Ramesh <Rohit507@gmail.com>
2014-10-01 10:11:21 -0400

    Added 'space*' tokens to make assignments work, now working on more complex rules.

--- parser.hs
+++ parser.hs
@@ -3,10 +3,13 @@
 {- To install the required package, and compile this app : 
  - Install the Haskell Platform : https://www.haskell.org/platform/
  - Get Peggy : $ cabal install peggy 
+ - Get Pretty-Show : $ cabal install pretty-show
  - Compile this app : ghc parser.hs
  -}
 
 import Text.Peggy
+import Text.Show.Pretty
+import Data.Maybe
 
 data Id = Dat String deriving (Show)    -- Identifiers used in the program
 
@@ -20,8 +23,8 @@ data Rule = Rule {                   -- A set of actions to perform when trigger
         actions :: [Action]
     } deriving (Show)
 
-data Event = Every Interval		            -- Event that procs every <interval> 
-	       | After Event Interval 	        -- Event that procs <interval> 
+data Event = Every Interval                 -- Event that procs every <interval> 
+	   | After Event Interval 	    -- Event that procs <interval> 
                                             --   after <event>
            | Begins Expression Interval     -- Event that happens when 
                                             --   <bool:Expression> goes from false to
@@ -39,6 +42,7 @@ data Action = Gather [Record] Table         -- Save a set of records into the
             | Message String                -- Send a message to controllers
             | Execute String                -- Run a piece of code             
             | Assign Assignment             -- Assign a particular value to an ID
+            | If Expression [Action]
             | AIdent Id                          
             deriving (Show)
 
@@ -65,6 +69,7 @@ data UnaryOp = UNot
              deriving (Show)
 
 data BinaryOp = BAnd | BOr | BEquals | BGrt | BLst | BGte | BLte | BNeq 
+              | BAdd | BSub | BDiv | BMul
               deriving (Show) 
 
 data Interval = Time Int 
@@ -86,6 +91,7 @@ data Record = Record {
 
 -- ############# Tokens and Simple Elements #################
 
+
 ident ::: Id = [a-z][0-9a-zA-Z_]* { Dat ($1 : $2)} 
 
 table ::: Table = [A-Z][0-9a-zA-Z_]* { Tbl ($1 : $2)} 
@@ -100,78 +106,94 @@ strn ::: String = '\"' [^""]* '\"' { $1 }
 
 intgr ::: Int = [0-9]+ {read $1}
 
+flot ::: Float = [0-9]+'.'[0-9]+   {read ($1 ++ "." ++ $2)}
+
+bol ::: Bool = 'True' {True}
+             / 'False' {False}
+
+
 -- ################# Program data and complex expressions ###############
 
-prog ::: Program = assignment* rule* { Program $1 $2 }
+prog :: Program = assignmentc* space* rule+  { Program $1 $3 }
 
-rule ::: Rule = 'ON' '(' event ')' '{' action* '}' { Rule $1 $2 } 
+rule :: Rule = 'ON' space* event space* '{' space* actionc* space* '}' space* 
+                                    { Rule $2 $5 } 
 
-event ::: Event = 'EVERY' interval { Every $1 } 
-                / interval 'AFTER' event { After $2 $1 }
-                / expression 'BEGINS' 'WHILE' 'CHECKING' interval { Begins $1 $2}
-                / expression 'ENDS' 'WHILE' 'CHECKING' interval 
-                                  { Begins (Unop UNot $1) $2 }
-                / event 'WITH' 'COOLDOWN' interval 
-                                  { Cooldown $1 $2 } 
-                / 'EVERY' interval 'WITHIN' interval 'AFTER' event
-                                  { Within $3 $1 $2 }
-                / '(' event ')'   { $1 }
+event :: Event = 'EVERY' interval { Every $1 } 
+               / interval space* 'AFTER' space* event { After $4 $1 }
+               / expression space* 'BEGINS' space* 'WHILE' space* 'CHECKING' space* 'EVERY' space* interval 
+                                    { Begins $1 $7}
+               / expression space* 'ENDS' space* 'WHILE' space* 'CHECKING' space* 'EVERY' space* interval 
+                                 { Begins (Unop UNot $1) $7 }
+               / 'PERFORM' space* event space* 'WITH' space* 'COOLDOWN' space* interval 
+                                 { Cooldown $2 $6 } 
+               / 'EVERY' space* interval space*  'WITHIN' space* interval space* 'AFTER' space* event
+                                 { Within $8 $2 $5 }
+               / '(' space* event space* ')'   { $2 }
 
-action ::: Action = 'GATHER' '{' records '}' 'INTO' table ';'   { Gather $1 $2 } 
-                  / 'SEND' strn ';'                           { Message $1 }
-                  / 'EXECUTE' call ';'                          { Execute $1 }
-                  / assignment                                  { Assign $1 }
-                  / ident ';'                                   { AIdent $1 }
+actionc :: Action = action space* ';' space* { $1 } 
 
-records ::: [Record] = record ',' records { $1 : $2 }
-                     / record { [$1] } 
+action :: Action = 'GATHER' space* '{' space* records space* '}' space* 'INTO' space* table 
+                    { Gather $3 $7 } 
+                 / 'SEND' strn                          { Message $1 }
+                 / 'EXECUTE' call                           { Execute $1 }
+                 / 'IF' space* expression space* '{' space* actionc* space* '}' { If $2 $5 } 
+                 / assignment                                   { Assign $1 }
+                 / ident                                    { AIdent $1 }
 
-record ::: Record = 'SAVE' expression 'AS' field { Record $1 $2 }
+records :: [Record] = record space* ',' space* records { $1 : $4 }
+                    / record { [$1] } 
 
-expression ::: Expression = '(' expression ')'  { $1 } 
-                          / interval            { Inte $1 }
-                          / call                { Call $1 }
-                          / strn                { Str $1 } 
-                          / [0-9]+              { Intg (read $1 ) } 
-                          / [0-9]+'.'[0-9]+     { Flt (read ($1 ++ $2))}
-                          / 'True'              { Bl True }
-                          / 'False'             { Bl False }
-                          / unop expression     { Unop $1 $2 } 
-                          / expression binop expression
-                                                { Binop $2 $1 $3 } 
-                          / ident               { EIdent $1 }
+record :: Record = 'SAVE' expression 'AS' field { Record $1 $2 }
 
-unop ::: UnaryOp = '!'          { UNot }
-                 / 'NOT'        { UNot }
+expression :: Expression = interval            { Inte $1 }
+                         / flot                { Flt $1}
+                         / intgr               { Intg $1 } 
+                         / call                { Call $1 }
+                         / strn                { Str $1 } 
+                         / bol                 { Bl $1 }
+                         / unop space* expression     { Unop $1 $3 } 
+                         / '(' space*  expression space* binop space* expression space* ')'
+                                               { Binop $4 $2 $6 } 
+                         / ident               { EIdent $1 }
+                         / '(' expression ')'  { $1 } 
+
+unop :: UnaryOp = '!'          { UNot }
+                / 'NOT'        { UNot }
+
+binop :: BinaryOp = '&&'  { BAnd }
+                  / 'AND' { BAnd }
+                  / '||'  { BOr }
+                  / 'OR'  { BOr }
+                  / '=='  { BEquals }
+                  / '>'   { BGrt }
+                  / 'GREATER' 'THAN' { BGrt }
+                  / '<'   { BLst }
+                  / 'LESS' 'THAN' { BLst }
+                  / '>='  { BGte }
+                  / 'GREATER' 'THAN' 'OR' 'EQUAL' { BGte }
+                  / '<='  { BLte }
+                  / 'LESS' 'THAN' 'OR' 'EQUAL' { BLte }
+                  / '!='  { BNeq }
+                  / 'NOT' 'EQUAL' { BNeq }
+                  / '+'  { BAdd }
+                  / '*'  { BMul }
+                  / '/'  { BDiv }
+                  / '-'  { BSub }
 
 
-binop ::: BinaryOp = '&&'  { BAnd }
-                   / 'AND' { BAnd }
-                   / '||'  { BOr }
-                   / 'OR'  { BOr }
-                   / '=='  { BEquals }
-                   / '>'   { BGrt }
-                   / 'GREATER' 'THAN' { BGrt }
-                   / '<'   { BLst }
-                   / 'LESS' 'THAN' { BLst }
-                   / '>='  { BGte }
-                   / 'GREATER' 'THAN' 'OR' 'EQUAL' { BGte }
-                   / '<='  { BLte }
-                   / 'LESS' 'THAN' 'OR' 'EQUAL' { BLte }
-                   / '!='  { BNeq }
-                   / 'NOT' 'EQUAL' { BNeq }
+interval :: Interval = timesec+   { Time $! foldl (+) 0 $1  }
 
+timesec :: Int = intgr 'min''s'?  { 60 * $1 } 
+               / intgr 'sec''s'?  { $1 }
+               / intgr 'hrs'      { 60 * 60 * $1 }
+               / intgr 'hour''s'?     { 60 * 60 * $1 }
+               / intgr 'day''s'?  { 60 * 60 * 24 * $1 } 
 
-interval ::: Interval = timesec+   { Time $ foldl (+) 0 $1  }
+assignmentc :: Assignment = assignment space* ';' space* { $1 }
 
-timesec ::: Int = intgr 'min''s'?  { 60 * $1 } 
-                / intgr 'sec''s'?  { $1 }
-                / intgr 'hrs'      { 60 * 60 * $1 }
-                / intgr 'hour'     { 60 * 60 * $1 }
-                / intgr 'day''s'?  { 60 * 60 * 24 * $1 } 
-
-assignment ::: Assignment = ident ':=' expression ';' { Assignment $1 (Easgn $2)}
-                          / ident ':=' action      { Assignment $1 (Aasgn $2)}
+assignment :: Assignment = ident ':=' space* expression  { Assignment $1 (Easgn $3)}
+                         / ident ':=' space* action      { Assignment $1 (Aasgn $3)}
                           
 
 |]
@@ -186,8 +208,10 @@ assignment ::: Assignment = ident ':=' expression ';' { Assignment $1 (Easgn $2)
  -
  - Float handling is not very good;
  -
- - Assignment syntax structure is weird and requires an ending ';' 
+ - there's no actual tokenization step so we've got 'space*' tokens *everywhere*
  -}
 
 main :: IO()
-main = print . parseString prog "<stdin>" =<< getContents
+main = putStr . ppShow . parseString prog "<stdin>" =<< getContents
+-- main = putStr . ppShow . parseString rule "<stdin>" =<< getContents
+
